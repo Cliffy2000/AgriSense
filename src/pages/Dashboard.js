615b21bbb4c9663ad6f5043d3d836b30d97de839
Cliffy2000 <cliffyyc@outlook.com>
2024-10-30 00:40:10 -0400

    update

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles//Dashboard.css';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="header">Header</div>
+      <div className="sidebar-left">Sidebar Left</div>
+      <div className="main-center">Main Center</div>
+      <div className="sidebar-right">Sidebar Right</div>
+    </div>
+  );
+};
+
+export default Dashboard;
